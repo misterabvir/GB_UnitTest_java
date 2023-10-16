b2d39dfd834fd01045b4a969152c5498cd14f947
misterabvir <to@misterabvir.ru>
2023-10-16 22:42:15 +0300

    hm3 create test checkValueIsEvent

--- GB_HM_unit_test/Test/HM3/HomeWork3MainTests.java
+++ GB_HM_unit_test/Test/HM3/HomeWork3MainTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HomeWork3MainTests {
@@ -15,9 +16,16 @@ public class HomeWork3MainTests {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = { 1,2,3 })
+    @ValueSource(ints = { 1,3,5 })
     void checkValueIsOdd(int input){
         boolean actual = service.evenOddNumber(input);
         assertTrue(actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 4,2,6 })
+    void checkValueIsEvent(int input){
+        boolean actual = service.evenOddNumber(input);
+        assertFalse(actual);
     }
 }
