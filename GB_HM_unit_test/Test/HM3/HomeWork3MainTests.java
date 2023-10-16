23882c3e7912ec79cad95fec3f1a699345cf4f0c
misterabvir <to@misterabvir.ru>
2023-10-16 22:40:47 +0300

    hm3 create method evenOddNumber

--- GB_HM_unit_test/Test/HM3/HomeWork3MainTests.java
+++ GB_HM_unit_test/Test/HM3/HomeWork3MainTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class HomeWork3MainTests {
 
     MainHW service;
