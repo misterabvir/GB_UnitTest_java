4aed7bb9ffe2f920931ac368c53c2cb986d51dcf
misterabvir <to@misterabvir.ru>
2023-10-16 23:19:26 +0300

    hm3 add field isAdmin in User class, add initialize field isAdmin in constructor

--- GB_HM_unit_test/src/HM3/UserData/User.java
+++ GB_HM_unit_test/src/HM3/UserData/User.java
@@ -6,12 +6,13 @@ public class User {
 
     String name;
     String password;
-
     boolean isAuthenticate = false;
+    boolean isAdmin;
 
     public User(String name, String password, boolean isAdmin) {
         this.name = name;
         this.password = password;
+        this.isAdmin = isAdmin;
     }
 
     //3.6.
